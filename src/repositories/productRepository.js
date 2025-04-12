038abe90fa2263b7d0fbddc70f6d48d644adf885
Gabriel Arturo Garcia <61440596+gabrielgarcia2211@users.noreply.github.com>
2025-04-12 13:05:44 -0500

    feat: formato de salida de JSON

--- src/repositories/productRepository.js
+++ src/repositories/productRepository.js
@@ -1,7 +1,18 @@
 const db = require("../config/database");
 
 const findActive = () => {
-  const query = "SELECT * FROM products WHERE activo = 1";
+  const query = `
+    SELECT 
+      products.nombre, 
+      CONCAT('$', FORMAT(products.precio, 2)) AS precio, 
+      products.descripcion, 
+      CASE 
+        WHEN products.activo = 1 THEN 'Activo' 
+        ELSE 'Inactivo' 
+      END AS estado
+    FROM products
+    WHERE products.activo = 1;
+  `;
 
   return new Promise((resolve, reject) => {
     db.query(query, (error, results) => {
